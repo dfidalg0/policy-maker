81cc948d5627a7ce6b21619d1158c167dffd66bc
Diego Fidalgo <diego-fidalgo@hotmail.com>
2022-10-09 02:49:32 -0300

    Mudança no formato de print do nó do tipo SyscallParam

--- src/syntax/nodes/syscall_param.cc
+++ src/syntax/nodes/syscall_param.cc
@@ -2,5 +2,11 @@
 
 void SyscallParam::print(uint level) {
     std::cout << indent(level) << "> SyscallParam: \n";
-    std::cout << indent(level + 2) << "- Name: " << _name << std::endl;
+
+    try {
+        std::cout << indent(level + 2) << "- Index: " << std::stoi(_name) << "\n";
+    }
+    catch (std::invalid_argument) {
+        std::cout << indent(level + 2) << "- Name: " << _name << std::endl;
+    }
 }
