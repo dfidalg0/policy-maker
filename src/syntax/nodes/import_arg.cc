d70c96e1344595b9ae046ee8cdb857acc76e0d37
Diego Fidalgo <diego-fidalgo@hotmail.com>
2022-10-16 16:31:27 -0300

    Melhoria no print de imports de políticas

--- src/syntax/nodes/import_arg.cc
+++ src/syntax/nodes/import_arg.cc
@@ -4,7 +4,14 @@ using namespace syntax;
 
 void ImportArg::print(uint level) {
     std::cout << indent(level) << "> ImportArg\n";
-    std::cout << indent(level + 2) << "- Name: " << _name << "\n";
+    std::cout << indent(level + 2) << "- Name: ";
+
+    if (_policy) {
+        std::cout << "policy ";
+    }
+
+    std::cout << _name << '\n';
+
     if (_aliased) {
         std::cout << indent(level + 2) << "- Alias: " << _alias << "\n";
     }
